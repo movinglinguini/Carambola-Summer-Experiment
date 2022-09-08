81596e222922dc3fdd22f13b27af2dff176f96b0
LAG1996 <luis.a.garcia01996@gmail.com>
2022-09-08 14:22:05 -0400

    make it so that action pairs never reappear

--- src/app/services/game-logic/modules/generate-decision-event.ts
+++ src/app/services/game-logic/modules/generate-decision-event.ts
@@ -8,6 +8,14 @@ export interface IDecisionEvent {
   chosenAction: IAction | null;
 }
 
+/** Tracks action pairs */
+const actionPairTracker = new Set<string>();
+let viableAction1s: IAction[];
+/**
+ * Tracks the amount of times each action has been used.
+ */
+const actionToUsesMap = new Map<string, number>();
+
 /**
  * Selects two alternatives for a player to choose from.
  *
@@ -16,11 +24,28 @@ export interface IDecisionEvent {
  */
 export function generateDecisionEvent(): IDecisionEvent {
   // choose two opposing values that the player must choose between to promote or harm
+  // repeat until we find a pairing
+
+  if (!viableAction1s) {
+    viableAction1s = GameResources.actionList;
+  }
+
   const value1Idx = Math.floor(Math.random() * VALUE_LIST.length);
-  const action1 = getAnActionWithAttitudeOnValue(ActionValueEffects.PROMOTE, value1Idx);
-  const otherActions = Array.from(actionMap.values()).filter(act => act.name !== action1.name);
+  const action1 = getAnActionWithAttitudeOnValue(viableAction1s, ActionValueEffects.PROMOTE, value1Idx);
+  const otherActions = viableAction1s
+    .filter(act => {
+      const noDupe = act.name !== action1.name;
+      const hasNotBeenPaired = !actionPairTracker.has(getActionPairKey(action1, act));
+      return noDupe && hasNotBeenPaired;
+    });
 
   const action2 = selectRandom(otherActions);
+
+  logActionPair(action1, action2);
+  incrementActionUsage(action1);
+  incrementActionUsage(action2);
+  tryDisqualifyAction(action1);
+  tryDisqualifyAction(action2);
 
   return {
     alternatives: [(action1 as IAction), (action2 as IAction)],
@@ -29,10 +54,48 @@ export function generateDecisionEvent(): IDecisionEvent {
 }
 
 /**
- * Helper function for
+ * Helper function for retrieving an action that effects the given value (`valueId`) with the given polarity (`effectKey`).
  */
-function getAnActionWithAttitudeOnValue(effectKey: ActionValueEffects, valueIdx: number): IAction {
-  return GameResources.actionList.find(action => {
+function getAnActionWithAttitudeOnValue(fromList: IAction[], effectKey: ActionValueEffects, valueIdx: number): IAction {
+  return fromList.find(action => {
     return action[effectKey].includes(valueIdx);
   }) as IAction;
 }
+
+function logActionPair(action1: IAction, action2: IAction) {
+  const actionPairKey = getActionPairKey(action1, action2);
+  actionPairTracker.add(actionPairKey);
+}
+
+function getActionPairKey(action1: IAction, action2: IAction) {
+  const sortedKeys = [action1.name, action2.name].sort((a, b) => a.localeCompare(b));
+  const pairKey = sortedKeys.join('.');
+  return pairKey;
+}
+
+function incrementActionUsage(action: IAction) {
+  if(!actionToUsesMap.has(action.name)) {
+    actionToUsesMap.set(action.name, 0);
+  }
+
+  const uses = actionToUsesMap.get(action.name) as number;
+  actionToUsesMap.set(action.name, uses + 1);
+}
+
+/**
+ * Checks if the action exceeded its usage threshold. If so, removes it from the list of
+ * viable actions to choose.
+ */
+function tryDisqualifyAction(action: IAction) {
+  /**
+   * The amount of times an action can appear before it is disqualified as a viable action.
+   */
+  const actionDequalThreshold = GameResources.actionList.length - 1;
+  const uses = actionToUsesMap.get(action.name) as number;
+
+  if (uses >= actionDequalThreshold) {
+    const actIdx = viableAction1s.findIndex(_action => _action.name === action.name);
+    viableAction1s.splice(actIdx, 1);
+  }
+}
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  testMode: false,
+  testMode: true,
   showAffinityTable: false,
   showRawNumbers: false,
   showLog: false,
